cebe0e0fbf5e0f5da8f96f19f00ed203f3f69d04
filonushka <lero4ka.filonova@mail.ru>
2020-11-01 00:45:00 +0300

    feat: finish dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function createDreamTeam(members) {
+  let dreamTeam = '';
+  let removedSpaces = [];
+  let sortedmembers = [];
+  let sanitized = [];
+  if (!Array.isArray(members)) {
+    return false;
+  } else {
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+    for (i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        sanitized.push(members[i].toUpperCase());
+      }
+    }
+
+    for (i = 0; i < sanitized.length; i++) {
+      removedSpaces.push(sanitized[i].replace(/\s/g, ''));
+    }
+
+    sortedmembers = removedSpaces.sort();
+    for (i = 0; i < sortedmembers.length; i++) {
+      dreamTeam += sortedmembers[i].charAt(0);
+    }
+    console.log(dreamTeam);
+    return dreamTeam;
+  }
 };
