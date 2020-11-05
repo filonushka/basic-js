394d4eec4a2b0d2130a3a69af156c374a4326767
filonushka <lero4ka.filonova@mail.ru>
2020-11-05 11:46:03 +0300

    feat: finish simple-chain, transform array

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,40 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.currChain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.currChain.push(`(${content})`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this._checkIntegerNumber(position)) {
+      if (position < 1 || position > this.getLength()) {
+        throw new Error('Removing link position is out of range!');
+      }
+      const index = position - 1;
+      this.currChain.splice(index, 1);
+      return this;
+    }
+    this._deleteChain();
+    throw new Error('Position must be integer number!');
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.currChain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const chain = this._getChain();
+    this._deleteChain();
+    return chain;
+  },
+  _deleteChain() {
+    this.currChain.length = 0;
+  },
+  _checkIntegerNumber(num) {
+    return typeof num === 'number' && (num ^ 0) === num;
+  },
+  _getChain() {
+    return this.currChain.join('~~');
   }
 };
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,71 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('Wrong argument!\'arr\'must be an Array!');
+
+  const clone = [...arr];
+  const [lastIndex, length] = [clone.lenght - 1, clone.length];
+  const res = [];
+
+  const actions = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev',
+  ];
+
+  const NA = '--empty--';
+
+  const specialValues = [...actions, NA];
+
+  for (let i = 0; i < length; i++) {
+    const curr = clone[i];
+
+    if (!specialValues.includes(curr)) {
+      res.push(curr);
+      continue;
+    }
+
+    switch (curr) {
+      case '--discard-next':
+        if (i >= lastIndex) {
+          continue;
+        } else {
+          clone[i + 1] = NA;
+          i++;
+        }
+        break;
+
+      case '--discard-prev':
+        if (i === 0 || (clone[i - 1] === NA)) {
+          continue;
+        } else {
+          res.pop();
+        }
+        break;
+
+      case '--double-next':
+        if (i >= lastIndex) {
+          continue;
+        } else {
+          res.push(clone[i + 1]);
+        }
+        break;
+
+      case '--double-prev':
+        if (i === 0 || (clone[i - 1] === NA)) {
+          continue;
+        } else {
+          res.push(clone[i - 1]);
+        }
+        break;
+
+      case NA:
+        continue;
+        break;
+
+    }
+
+  }
+  return res;
 };
